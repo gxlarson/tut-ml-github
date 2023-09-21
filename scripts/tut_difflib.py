d11b95ef6db91f291188bc1d204ae4569ae9e2cf
gxlarson <gxlarson@gmail.com>
2023-09-21 13:05:31 -0400

    Update tut_difflib.py

--- scripts/tut_difflib.py
+++ scripts/tut_difflib.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from difflib import SequenceMatcher
 
 s1 = "this is the second document"
@@ -7,5 +6,4 @@ s2 = "this um is the second document"
 d = SequenceMatcher(a=s1.split(), b=s2.split())
 
 r = d.ratio()
-
-pprint(r)
+print(r)
